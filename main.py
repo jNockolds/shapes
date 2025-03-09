2dea1a2e0a8f023a9ea1ff75a98ca442477cad32
JossNockolds <19121867@hlnsc.ac.uk>
2025-03-09 22:02:14 +0000

    implemented classes for shapes, implemented a time measurer for functions, implemented angle offsets, implemented custom lists of shapes to be nested

--- main.py
+++ main.py
@@ -1,124 +1,275 @@
-"""Todo:
-- implement angle_offset
-"""
-
 import turtle
 import numpy as np
+import time
 
-# initial definitions: 
+"""Todo:
+- change how ending_sides is set to ensure it works with density
+"""
 
-## screen:
+# default values:
 
-screen = turtle.Screen()
-screen.title('Shapes')
-screen_width = 800
-screen.bgcolor("#100026")
-screen.setup(width=screen_width, height=screen_width)
+time_decimal_places = 6
 
-## turtle:
+d_screen_name = "Shape drawer"
+d_screen_width = 800
+d_bg_colour = "#100026"
 
-pen = turtle.Turtle()
-pen.hideturtle()
-pen.radians()
-pen.pensize(2)
-pen.pencolor("#A0A0A0")
+d_pen_colour = "#A0A0A0"
+d_pen_size = 2
+d_pen_speed = -1 # increasing above 10 has no additional effect
 
-## misc. variables:
+d_density = 1
+d_shape_centre = np.array([0,0])
+d_shape_angle_offset = np.pi/2
+d_precision = 0.1
 
-default_circle_precision= 0.1
+# decorator definitions:
 
-draw_is_instant = True
-if draw_is_instant == True:
-    screen.tracer(0)
-else:
-    pen.speed(0)
-
-## functions:
-
-def draw_polygon(radius, sides, density=1, centre=np.array([0,0]), angle_offset=0):
-    """Draws the largest regular polygon with a given number of sides that fits within a circle with a given radius. 
-    \n Parameters:
-    - density - the number of edges "skipped" + 1 (e.g. a pentagon's is 1, but a pentagram's is 2) (see Wikipedia for a more in-depth explanation: https://en.wikipedia.org/wiki/Density_(polytope)#Polygons)
-    - angle_offset - the amount the polygon is rotated anticlockwise about its centre (in radians)
+def measure_time(function):
+    """A decorator which measures the time it takes for a function to execute, printing the result. 
+    \n It also sums the total execution time of all functions.
     """
-    # handling parameters:
-    if radius <= 0:
-        raise ValueError("radius must be > 0")
-    if sides < (2 * density) + 1:
-        raise ValueError("sides must be > (2 * density) + 1")
-    if density < 1:
-        raise ValueError("density must be > 1")
-    
-    # drawing:
-    pen.up()
-    pen.goto(centre[0] + radius, centre[1])
-    pen.down()
-    i = 0
-    while i < sides:
-        # moving to home vertex
-        pen.up()
-        pen_x = radius * np.cos((2 * i * np.pi) / sides)
-        pen_y = radius * np.sin((2 * i * np.pi) / sides)
-        pen.goto(pen_x, pen_y) # home vertex
+    def wrapper(*args, **kwargs): # *args and **kwargs allow the function to take in parameters
+        # timing the function:
+        start = time.time()
+        function(*args, **kwargs)
+        end = time.time()
 
-        # connecting to the vertex forward by an number of vertexs equal to density
-        i += density
-        pen_x = radius * np.cos((2 * i * np.pi) / sides)
-        pen_y = radius * np.sin((2 * i * np.pi) / sides)
-        pen.down()
-        pen.goto(pen_x, pen_y) # destination vertex
+        # gettting results:
+        elapsed_time = end - start
+        function_name = function.__qualname__ # __qualname__ gives class_name.function_name
+
+        print(f'{function_name} execution time: {round(elapsed_time, time_decimal_places)} seconds') # __name__ needed to give just the name, rather than the object type, name, and ram location
+    return wrapper
+
+# class definitions:
+
+class Drawer:
+    """Encapsulates the screen and turtle into one class."""
+    def __init__(self, screen_width=d_screen_width, bg_colour=d_bg_colour, pen_colour=d_pen_colour, pen_size=d_pen_size, pen_speed=d_pen_speed, screen_name=d_screen_name):
+        # screen setup:
+        self.screen_width = screen_width
+        self.screen = turtle.Screen()
+        self.screen.title(screen_name)
+        self.screen.bgcolor(bg_colour)
+        self.screen.setup(width=screen_width, height=screen_width)
         
-        i -= density - 1 # set index of new home vertex (1 after the previous home vertex)
+        # turtle setup:
+        self.pen = turtle.Turtle()
+        self.pen.hideturtle()
+        self.pen.radians()
+        self.pen.pensize(pen_size)
+        self.pen.pencolor(pen_colour)
+
+        # pen speed:
+        if pen_speed == -1:
+            self.screen.tracer(0)
+        else:
+            self.pen.speed(pen_speed)
     
-def draw_circle(radius, centre=np.array([0,0]), precision=0.1):
-    """Draws a circle with a given radius. 
-    \n Parameters:
+    def update(self):
+        self.screen.update()
+
+    def done(self):
+        turtle.done()
+
+class Shape:
+    """Superclass for specific shape subclasses."""
+    def __init__(self, radius, centre=d_shape_centre):
+        # checking attributes:
+        if radius <= 0:
+            raise ValueError("radius must be > 0.")
+        
+        # assigning attributes:
+        self.radius = radius
+        self.centre = centre
+    
+    @measure_time
+    def draw(self, pen):
+        raise NotImplementedError("Subclasses must implement this method.")
+    
+class RegularPolygon(Shape):
+    """A regular polygon with its size defined by the smallest radius of circle it can fit within.
+    \n Attributes:
+    - density - the number of edges "skipped" + 1 (e.g. a pentagon's is 1, but a pentagram's is 2); density <= (sides - 1) / 2 (see Wikipedia for a more in-depth explanation: https://en.wikipedia.org/wiki/Density_(polytope)#Polygons)
+    - angle_offset - the amount the polygon is rotated anticlockwise about its centre (in radians)
+    """
+    def __init__(self, radius, sides : int, density=d_density, centre=d_shape_centre, angle_offset=d_shape_angle_offset):
+        # checking attributes:
+        if sides < (2 * density) + 1:
+            raise ValueError("sides must be >= (2 * density) + 1.")
+        if density < 1:
+            raise ValueError("density must be >= 1.")
+    
+        # assigning attributes:
+        super().__init__(radius, centre)
+        self.sides = sides
+        self.density = density
+        self.angle_offset = angle_offset
+    
+    @measure_time
+    def draw(self, pen):
+        # moving to first vertex:
+        pen.up()
+        pen.goto(self.centre[0] + self.radius, self.centre[1])
+        pen.down()
+
+        # drawing lines between vertices:
+        i = 0
+        while i < self.sides:
+            # moving to home vertex:
+            pen.up()
+            home_angle = ((2 * i * np.pi) / self.sides) + self.angle_offset
+            home_x = self.radius * np.cos(home_angle)
+            home_y = self.radius * np.sin(home_angle)
+            pen.goto(home_x, home_y) # home vertex
+
+            # moving to destination vertex, skipping a number of vertexes equal to density - 1:
+            i += self.density
+            destination_angle = ((2 * i * np.pi) / self.sides) + self.angle_offset
+            destination_x = self.radius * np.cos(destination_angle)
+            destination_y = self.radius * np.sin(destination_angle)
+            pen.down()
+            pen.goto(destination_x, destination_y) # destination vertex
+            
+            # set i to new home vertex (1 after the previous home vertex):
+            i -= self.density - 1
+
+class Circle(Shape):
+    """A circle approximated by a regular polygon.
+    \n Attributes:
     - precision - the angular step between vertexes; a smaller value gives a smoother circle
     """
-    # handling parameter:
-    if precision <= 0:
-        raise ValueError("precision must be > 0")
+    def __init__(self, radius, centre=d_shape_centre, precision=d_precision):
+        # checking attributes:
+        if precision <= 0:
+            raise ValueError("precision must be > 0.")
     
-    # drawing:
-    sides = round((2 * np.pi) / precision)
-    draw_polygon(radius, sides, centre=centre)
+        # assigning attributes:
+        super().__init__(radius, centre)
+        self.precision = precision
 
-def draw_circscribed_polygon(radius, sides, density=1, centre=np.array([0,0]), angle_offset=0, precision=default_circle_precision):
-    """Draws a circle with a given radius and the largest regular polygon with a given number of sides that fits within it. 
-    \n Parameters:
-    - density - the number of edges "skipped" + 1 (e.g. a pentagon's is 1, but a pentagram's is 2) (see Wikipedia for a more in-depth explanation: https://en.wikipedia.org/wiki/Density_(polytope)#Polygons)
+    @measure_time
+    def draw(self, pen):
+        sides = round((2 * np.pi) / self.precision) # number of sides for the circle approximation
+        poly = RegularPolygon(self.radius, sides, centre=self.centre, angle_offset=0)
+        poly.draw(pen)
+
+class CircumscribedRegularPolygon(RegularPolygon, Circle):
+    """Draws a circle with a given radius and the largest regular polygon with a given number of sides that fits within it.
+    \n Attributes:
+    - density - the number of edges "skipped" + 1 (e.g. a pentagon's is 1, but a pentagram's is 2); density <= (sides - 1) / 2 (see Wikipedia for a more in-depth explanation: https://en.wikipedia.org/wiki/Density_(polytope)#Polygons)
     - angle_offset - the amount the polygon is rotated anticlockwise about its centre (in radians)
     - precision - the angular step between vertexes; a smaller value gives a smoother circle
     """
-    draw_circle(radius, centre, precision)
-    draw_polygon(radius, sides, density, centre, angle_offset)
+    def __init__(self, radius, sides, density=d_density, centre=d_shape_centre, angle_offset=d_shape_angle_offset, precision=d_precision):
+        RegularPolygon.__init__(self, radius, sides, density, centre, angle_offset)
+        Circle.__init__(self, radius, centre, precision)
 
-def draw_nested_circscribed_polygons(radius, sides, density=1, centre=np.array([0,0]), angle_offset=0, precision=default_circle_precision, ending_sides=3):
-    """Draws a circle with a given radius and the largest regular polygon with a given number of sides that fits within it. 
-    \n Parameters:
-    - density - the number of edges "skipped" + 1 (e.g. a pentagon's is 1, but a pentagram's is 2) (see Wikipedia for a more in-depth explanation: https://en.wikipedia.org/wiki/Density_(polytope)#Polygons)
-    - angle_offset - the amount the polygon is rotated anticlockwise about its centre (in radians)
-    - precision - the angular step between vertexes; a smaller value gives a smoother circle
-    - ending_sides - the number of sides the polygon with the least number of sides has
+    @measure_time
+    def draw(self, pen):
+        circle = Circle(self.radius, self.centre, self.precision)
+        regular_polygon = RegularPolygon(self.radius, self.sides, self.density, self.centre, self.angle_offset)
+
+        circle.draw(pen)
+        regular_polygon.draw(pen)
+
+class NestedCircumscribedRegularPolygons(Shape):
+    """Nests circumscribed polygons in each other, decreasing in number of sides until it reaches a minimum.
+    \n Attributes:
+    - layers - the number of polygons in the nested shape (note that circles aren't polygons)
+    - circumscribedRegularPolygons - the list containing all of the polygons
     """
-    radius_ratio = 1
-    ending_sides = (2 * density) + 1
-    for side_num in range(sides, ending_sides - 1, -1):
-        draw_circscribed_polygon(radius * radius_ratio, side_num, density, centre, angle_offset, precision)
-        radius_ratio *= np.cos((np.pi * density) / side_num)
+    # constructors:
+    
+    ## one specific CircumscribedRegularPolygon:
+    def __init__(self, circumscribedRegularPolygon, layers=2):
+        self.layers = layers
+        self.circumscribedRegularPolygons = []
+        for i in range(layers):
+            self.circumscribedRegularPolygons.append(circumscribedRegularPolygon)
 
-# drawing: 
+    ## one specific RegularPolygon:
+    def __init__(self, regularPolygon, layers=2, precision=d_precision):
+        # making the polygon circumscribed:
+        radius = regularPolygon.radius
+        sides = regularPolygon.sides
+        density = regularPolygon.density
+        centre = regularPolygon.centre
+        angle_offset = regularPolygon.angle_offset
+        circumscribedRegularPolygon = CircumscribedRegularPolygon(radius, sides, density, centre, angle_offset, precision)
 
-## shape variables:
+        # assigning attributes: 
+        self.layers = layers
+        self.circumscribedRegularPolygons = []
+        for i in range(layers):
+            self.circumscribedRegularPolygons.append(circumscribedRegularPolygon)
+        
+    ## a list of CircumscribedRegularPolygons:
+    def __init__(self, circumscribedRegularPolygons : list):
+        self.layers = len(circumscribedRegularPolygons)
+        self.circumscribedRegularPolygons = circumscribedRegularPolygons
+    
+    ## a list of RegularPolygons:
+    def __init__(self, regularPolygons : list, precision=d_precision):
+        self.layers = len(regularPolygons)
+        self.circumscribedRegularPolygons = []
+        for i in range(self.layers):
+            # making the polygon circumscribed:
+            radius = regularPolygons[i].radius
+            sides = regularPolygons[i].sides
+            density = regularPolygons[i].density
+            centre = regularPolygons[i].centre
+            angle_offset = regularPolygons[i].angle_offset
+            self.circumscribedRegularPolygons.append(CircumscribedRegularPolygon(radius, sides, density, centre, angle_offset, precision))
 
-radius = (screen_width // 2) - 10
-density = 2
-sides = 9
+    @measure_time
+    def draw(self, pen):
+        radius_ratio = 1
+        for i in range(self.layers):
+            # decreasing radius to fit within previous circle (unless radius_ratio == 1):
+            self.circumscribedRegularPolygons[i].radius *= radius_ratio
 
-## calling functions:
+            self.circumscribedRegularPolygons[i].draw(pen)
 
-draw_nested_circscribed_polygons(radius, sides, density)
+            # updating raidus ratio for the next iteration:
+            radius_ratio *= np.cos((np.pi * self.circumscribedRegularPolygons[i].density) / self.circumscribedRegularPolygons[i].sides)
 
-screen.update() # renders the drawings
+# variables:
 
-turtle.done() # keeps screen open after drawing is complete
+drawer = Drawer()
+
+radius = (drawer.screen_width // 2) - 10
+sides = 5
+
+# initiating elapsed time period:
+start_time = time.time()
+
+# constructing shapes:
+
+circumscribed_regular_polygon1 = RegularPolygon(radius, 3)
+circumscribed_regular_polygon2 = RegularPolygon(radius, 4)
+circumscribed_regular_polygon3 = RegularPolygon(radius, 100)
+
+
+circumscribed_regular_polygons = [circumscribed_regular_polygon1, circumscribed_regular_polygon2, circumscribed_regular_polygon3]
+
+nested_circumscribed_regular_polygons = NestedCircumscribedRegularPolygons(circumscribed_regular_polygons)
+
+
+
+
+
+
+## calling draw functions:
+nested_circumscribed_regular_polygons.draw(drawer.pen)
+
+# rendering the drawings:
+drawer.update()
+
+# calculating and printing total elapsed time: 
+end_time = time.time()
+total_elapsed_time = end_time - start_time
+print(f'\nTotal execution time: {round(total_elapsed_time, time_decimal_places)} seconds')
+
+drawer.done() # keeps screen open after drawing is complete
